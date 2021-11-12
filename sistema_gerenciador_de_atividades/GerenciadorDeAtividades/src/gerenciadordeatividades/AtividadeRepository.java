67fb68be99b05f455adca28a1936b8bb5a130962
Janaína Madeira Tannus Teixeira <61756665+Janainateixeira@users.noreply.github.com>
2021-11-11 23:07:37 -0300

    Atualizando a implementação da listagem de banco de dados

--- sistema_gerenciador_de_atividades/GerenciadorDeAtividades/src/gerenciadordeatividades/AtividadeRepository.java
+++ sistema_gerenciador_de_atividades/GerenciadorDeAtividades/src/gerenciadordeatividades/AtividadeRepository.java
@@ -7,8 +7,10 @@ package gerenciadordeatividades;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Classe que mantém a entidade Atividade no banco de dados
@@ -18,11 +20,11 @@ import java.sql.SQLException;
 public class AtividadeRepository {
 
     public void inserir(Atividade a) {
-        
+
         try {
             Connection cx = ConnectionFactory.createConnection();
             PreparedStatement ps = cx.prepareStatement("insert into atividade (Descricao) values (?)");
-            ps.setString(1,a.getDescricao());
+            ps.setString(1, a.getDescricao());
             ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,7 +33,25 @@ public class AtividadeRepository {
     }
 
     public List<Atividade> listar() {
-        //TODO: implentar alistagem de atividade do banco de dados
-        return null;
+        List<Atividade> atividadesLidasDoBanco = new ArrayList<Atividade>();
+
+        try {
+            Connection cx = ConnectionFactory.createConnection();
+            PreparedStatement stmt = cx.prepareStatement("select * from atividade");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                // criando o objeto Atividade
+                Atividade a = new Atividade();
+                a.setId((int) rs.getLong("Id"));
+                a.setDescricao(rs.getString("Descricao"));
+                atividadesLidasDoBanco.add(a);
+            }
+            rs.close();
+            stmt.close();
+            return atividadesLidasDoBanco;
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
